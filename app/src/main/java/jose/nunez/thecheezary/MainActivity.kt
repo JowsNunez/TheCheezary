f85de5f63b7e1ad55bff7ecdd953f48c44d4d1b3
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-02-18 10:22:06 -0700

    Get Started y login

---
+++ app/src/main/java/jose/nunez/thecheezary/MainActivity.kt
@@ -0,0 +1,28 @@
+package jose.nunez.thecheezary
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val button:Button=findViewById(R.id.get_stared_button) as Button
+
+        button.setOnClickListener{
+            var intent: Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ app/src/main/java/jose/nunez/thecheezary/MenuActivity.kt
@@ -0,0 +1,11 @@
+package jose.nunez.thecheezary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
