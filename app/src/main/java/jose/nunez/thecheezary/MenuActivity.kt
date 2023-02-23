bb7f383d5bebab0e32ecbdbec80add28c9f0d95f
Jose Alfredo <jose.nunez181930@potros.itson.edu.mx>
2023-02-23 12:55:16 -0700

    create Activity Products

--- app/src/main/java/jose/nunez/thecheezary/MenuActivity.kt
+++ app/src/main/java/jose/nunez/thecheezary/MenuActivity.kt
@@ -1,11 +1,25 @@
 package jose.nunez.thecheezary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+        var btnColdDrinks: Button =findViewById(R.id.cold_drinks_button)
+
+        btnColdDrinks.setOnClickListener{
+
+            var intent: Intent = Intent(this,ProductsActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/jose/nunez/thecheezary/Product.kt
@@ -0,0 +1,6 @@
+package jose.nunez.thecheezary
+
+data class Product (var name:String ,
+                    var image:Int,
+                    var description: String,
+                    var price: Double)
